06862f1f373042853ae46953ee140b77c54f63f2
Abdulaxad7 <145189911+Abdulaxad7@users.noreply.github.com>
2024-07-11 13:12:11 +0500

    Update config.go

--- config/config/config.go
+++ config/config/config.go
@@ -11,9 +11,9 @@ func DB() Db {
 	return Db{
 		PublicHost: gETEnv("PUBLIC_HOST", "https://localhost"),
 		Port:       gETEnv("PORT", "8080"),
-		DBUser:     gETEnv("DBUSER", "root"),
-		DBAddr:     fmt.Sprintf("%s:%s", gETEnv("DB_HOST", "127.0.0.1"), gETEnv("DB_POST", "3306")),
-		DBPassword: gETEnv("DBPASSWORD", "0987poiulkjh"),
+		DBUser:     gETEnv("DBUSER", "**********"),
+		DBAddr:     fmt.Sprintf("%s:%s", gETEnv("DB_HOST", "***************"), gETEnv("DB_POST", "***************")),
+		DBPassword: gETEnv("DBPASSWORD", "**********"),
 		DBName:     gETEnv("DBNAME", "data12"),
 	}
 }
